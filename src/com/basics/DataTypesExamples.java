e3f94ae8ed06554bbbe49765504f433e58925ef4
Manojkumar026 <manojdurai1997@gmail.com>
2018-05-29 16:14:03 +0530

    basicprogram

---
+++ src/com/basics/DataTypesExamples.java
@@ -0,0 +1,11 @@
+package com.basics;
+
+public class DataTypesExamples {
+	public static void main(String[]args) {
+		String Firstname="Manoj";
+		String Secondname="kumar";
+		System.out.println(Firstname+Secondname);
+	}
+	
+
+}
