e24305ccc9ed44b7c139c6847cf1bcb8b87ebdee
Manojkumar026 <manojdurai1997@gmail.com>
2018-05-31 16:14:02 +0530

    creating date

---
+++ src/com/basics/Admin.java
@@ -0,0 +1,13 @@
+package com.basics;
+
+public class Admin {
+	public static void main(String args[]) {
+		Date d1 = new Date();
+		d1.setDay(31);
+		d1.setMonth(5);
+		d1.setYear(2018);
+		d1.displayDate();
+		
+	}
+
+}
